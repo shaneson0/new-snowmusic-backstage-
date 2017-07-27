e7549f86822d0074567c6db25267836eadb5c228
wzhx <1219251709@qq.com>
2017-07-27 15:34:38 +0800

    修改了 MV.vue 文件

--- src/components/views/MV.vue
+++ src/components/views/MV.vue
@@ -2,9 +2,14 @@
   <!-- Main content -->
   <div class="row center-block">
 
-    <pagenation :hasimage1="hasimage" :keys1="keys" :values1="values">
-
-    </pagenation>
+    <pagenation :hasimage1="hasimage" :keys1="keys" :values1="values" :EditRoute='editRoute'
+                v-on:searchfunction="searchfunction1" v-on:changePage="changepage1"></pagenation>
+    <div class="col-md-12">
+      <!--<router-link to="/updateSong"><i class="fa fa-fw fa-edit"></i></router-link>-->
+      <router-link to="/songDetail">
+        <button type="submit" class="btn btn-primary pull-right">增加MV</button>
+      </router-link>
+    </div>
   </div>
 
 </template>
@@ -22,31 +27,48 @@
     name: 'Tables',
     data: function () {
       return {
-        hasimage: 0,
+        hasimage: 1,
         keys: ['名字', '歌手', '语系', '专辑', '发布时间', '播放次数', '精选', '操作'],
-        values: []
+        values: [],
+        editRoute: '/updateMV'
+      }
+    },
+    methods: {
+      changepage1: function (from) {
+        alert(from)
+        this.getMVsList(10, from, '')
+      },
+      searchfunction1: function (Msg) {
+        this.getMVsList(10, 0, Msg)
+      },
+      getMVsList: function (limit, from, vague) {
+//        let data = `table=songslib&keyname=songname&queryfiles=songname,type,lang,singername,albumname,price,image,songid&from=${from}&limit=${limit}&keyvalue=${vague}`
+        var self = this
+        const data = `limit=${limit}&from=${from}&keyvalue=${vague}`
+        API.request('post', '/admin/mv/list', data).then(function (res) {
+          const Data = res.data.data
+          console.log('--------------------------')
+          console.log(Data)
+          console.log('++++++++++++++++++++++++++++')
+          const mvs = Data.mvs
+          const TempArray = []
+          for (var i = 0; i < mvs.length; i++) {
+            const mv = mvs[i]
+            const temp = {
+              image: mvs[i].image,
+              lists: [mv.mvname, mv.singername, mv.lang, mv.albumname, mv.create_ts, mv.play, '是'],
+              id: mvs[i].mvid
+            }
+            TempArray[i] = temp
+          }
+          self.values = TempArray
+        })
+//        console.log(self.values)
+//        console.log('End')
       }
     },
     created: function () {
-      console.log('Hello')
-      var self = this
-      const data = 'limit=15'
-      API.request('post', '/admin/mv/list', data).then(function (res) {
-        const Data = res.data.data
-        const mvs = Data.mvs
-        const TempArray = []
-        for (var i = 0; i < mvs.length; i++) {
-          const mv = mvs[i]
-          const temp = {
-            image: mvs[i].image,
-            lists: [mv.mvname, mv.singername, mv.lang, mv.albumname, mv.create_ts, mv.play, '是']
-          }
-          TempArray[i] = temp
-        }
-        self.values = TempArray
-      })
-      console.log(self.values)
-      console.log('End')
+      this.getMVsList(10, 0, '')
     },
     mounted () {
       this.$nextTick(() => {
@@ -71,7 +93,6 @@
 
   @import url('/static/js/plugins/datatables/jquery.dataTables.min.css');
   */
-
   @import url('/static/js/plugins/datatables/dataTables.bootstrap.css');
 
   table.dataTable thead .sorting:after,
