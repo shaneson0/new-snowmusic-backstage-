0b915f7c1ff3f63dbf4b3b409185028a23962cd5
shanxuan <781244184@qq.com>
2017-07-28 15:00:36 +0800

    修复btn样式覆盖问题

--- src/components/views/upload.vue
+++ src/components/views/upload.vue
@@ -25,8 +25,8 @@
 
         <div class="box-footer">
           <div id="container" class="pull-right">
-            <a id="selectfiles" href="javascript:void(0);" class='btn'>选择文件</a>
-            <a id="postfiles" href="javascript:void(0);" class='btn'>开始上传</a>
+            <a id="selectfiles" href="javascript:void(0);" class='btn btn-primary'>选择文件</a>
+            <a id="postfiles" href="javascript:void(0);" class='btn btn-primary'>开始上传</a>
           </div>
 
         </div>
@@ -50,31 +50,6 @@
 
 
 <style type="text/css">
-  .btn{
-    color: #fff;
-    background-color: #337ab7;
-    border-color: #2e6da4;
-    display: inline-block;
-    padding: 6px 12px;
-    margin-bottom: 0;
-    font-size: 14px;
-    font-weight: 400;
-    line-height: 1.42857143;
-    text-align: center;
-    white-space: nowrap;
-    text-decoration: none;
-    vertical-align: middle;
-    -ms-touch-action: manipulation;
-    touch-action: manipulation;
-    cursor: pointer;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-    background-image: none;
-    border: 1px solid transparent;
-    border-radius: 4px;
-  }
   a.btn:hover{
     background-color: #3366b7;
   }
