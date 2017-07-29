5984c90d3647a1891816b0f8364997a89b8cc774
wzhx <1219251709@qq.com>
2017-07-29 13:14:10 +0800

    modified sidebarmenu.vue

--- src/components/SidebarMenu.vue
+++ src/components/SidebarMenu.vue
@@ -8,7 +8,7 @@
     </li>
 
     <!--管理员-->
-    <li class="header" >Mangerments</li>
+    <li class="header">Mangerments</li>
     <li class="pageLink" v-on:click="toggleMenu">
       <router-link to="/Manager"><i class="fa fa-table"></i>
         <span class="page">管理员</span>
@@ -16,7 +16,7 @@
     </li>
 
     <!--管理员-->
-    <li class="header" >Mangerments</li>
+    <li class="header">Mangerments</li>
     <li class="pageLink" v-on:click="toggleMenu">
       <router-link to="/upload"><i class="fa fa-table"></i>
         <span class="page">上传</span>
@@ -24,7 +24,7 @@
     </li>
 
     <!--歌手，歌曲和音乐人-->
-    <li class="header" >Medias</li>
+    <li class="header">Medias</li>
     <li class=" pageLink" v-on:click="toggleMenu">
       <router-link to="/Singer"><i class="fa fa-table"></i>
         <span class="page">歌手和音乐人</span>
@@ -37,7 +37,7 @@
     </li>
 
     <!--Mv和专辑-->
-    <li class="header" >Medias</li>
+    <li class="header">Medias</li>
     <li class=" pageLink" v-on:click="toggleMenu">
       <router-link to="/MV"><i class="fa fa-table"></i>
         <span class="page">MV</span>
@@ -50,19 +50,19 @@
     </li>
 
     <!--排行榜-->
-    <li class="header" >排行榜</li>
+    <li class="header">排行榜</li>
     <li class=" pageLink" v-on:click="toggleMenu">
-      <router-link to="/rank"><i class="fa fa-table"></i>
+      <router-link to="/selectedRank"><i class="fa fa-table"></i>
         <span class="page">精选榜</span>
       </router-link>
     </li>
     <li class=" pageLink" v-on:click="toggleMenu">
-      <router-link to="/rank"><i class="fa fa-table"></i>
+      <router-link to="/hotSongsRank"><i class="fa fa-table"></i>
         <span class="page">热门榜</span>
       </router-link>
     </li>
     <li class=" pageLink" v-on:click="toggleMenu">
-      <router-link to="/rank"><i class="fa fa-table"></i>
+      <router-link to="/newSongsRank"><i class="fa fa-table"></i>
         <span class="page">最新版</span>
       </router-link>
     </li>
@@ -93,30 +93,30 @@
   </ul>
 </template>
 <script>
-export default {
-  name: 'SidebarName',
-  methods: {
-    toggleMenu (event) {
-      // remove active from li
-      var active = document.querySelector('li.pageLink.active')
+  export default {
+    name: 'SidebarName',
+    methods: {
+      toggleMenu (event) {
+        // remove active from li
+        var active = document.querySelector('li.pageLink.active')
 
-      if (active) {
-        active.classList.remove('active')
+        if (active) {
+          active.classList.remove('active')
+        }
+        // window.$('li.pageLink.active').removeClass('active')
+        // Add it to the item that was clicked
+        event.toElement.parentElement.className = 'pageLink active'
       }
-      // window.$('li.pageLink.active').removeClass('active')
-      // Add it to the item that was clicked
-      event.toElement.parentElement.className = 'pageLink active'
     }
   }
-}
 </script>
 <style>
   /* override default */
-  .sidebar-menu>li>a {
+  .sidebar-menu > li > a {
     padding: 12px 15px 12px 15px;
   }
 
-  .sidebar-menu li.active>a>.fa-angle-left, .sidebar-menu li.active>a>.pull-right-container>.fa-angle-left {
+  .sidebar-menu li.active > a > .fa-angle-left, .sidebar-menu li.active > a > .pull-right-container > .fa-angle-left {
     animation-name: rotate;
     animation-duration: .2s;
     animation-fill-mode: forwards;
